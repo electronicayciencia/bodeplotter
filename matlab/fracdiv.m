34d3b664e823e29508981f612b7954ae617266c8
Electronica y ciencia <electronicayciencia@gmail.com>
2017-03-26 14:35:05 +0200

    Added some plots as examples.

--- matlab/fracdiv.m
+++ matlab/fracdiv.m
@@ -4,15 +4,15 @@
 
 %-------------------------------------
 % Main options
-Fc    = 500;    % clock frequency
-Tfreq = 108.75; % Target frequency
-MASH  = 8;      % MASH stages filter level
+Fc    = 500;     % clock frequency
+Tfreq = 27.155;  % Target frequency
+Order = 8;       % Fractional order
 
 DIV  = Fc / Tfreq;
 
 %-------------------------------------
 % Main loop
-n  = 1000000;  % number of points
+n  = 2000000;  % number of points
 SR = 20*Fc;    % sampling rate
 T  = n/SR;     % sampling time (s)
 
@@ -23,7 +23,7 @@ t = 0:1/SR:T-1/SR; % time vector
 % double the frequency so we get Fc pulses per second after divide by 1
 w = square(2*pi*2*Fc*t);
 
-if (MASH > 0)
+if (Order > 0)
     icounter_max = floor(DIV);
 else
     icounter_max = round(DIV);
@@ -32,9 +32,9 @@ end
 % remainder of fractional divider
 rem = mod(Fc,Fc/DIV);
 
-fcounter_max = MASH * (Fc-rem)/rem;
+fcounter_max = Order * (Fc-rem)/rem;
 fcounter_max = round(fcounter_max);
-fprintf ('Swallow %d every %d source pulses\n', MASH, fcounter_max);
+fprintf ('Swallow %d every %d source pulses\n', Order, fcounter_max);
 
 out = zeros(1,n);
 
@@ -48,9 +48,9 @@ for i = 1:length(w);
     v = w(i);
     
     if (v == 1) && (v ~= last_v) % raising edge
-        if (MASH > 0) && (fcounter >= fcounter_max)
+        if (Order > 0) && (fcounter >= fcounter_max)
             mcounter = mcounter + 1;
-            if (mcounter >= MASH)
+            if (mcounter >= Order)
                 mcounter = 0;
                 fcounter = 0;
             end
@@ -80,29 +80,28 @@ ff = ff(2:n/2);
 [val,pos] = max(ff);
 peakfreq = ft(pos);
 
+meanfreq = sum(diff(out) < 0)/T;
 
-meanfreq = sum(abs( ...
-    out(2:length(out)) - out(1:length(out)-1) ...
-    )) /2 / T;
 
-fprintf('Target frequency: %4.4fHz\n', Tfreq)
-
-fprintf('Main frequency:   %4.4fHz (%4.2fdB)\n', ...
+s1   = sprintf('Source frequency: %4.3fHz', Fc);
+s2   = sprintf('Fractional order: %d', Order);
+str1 = sprintf('Target frequency: %4.3fHz', Tfreq);
+str2 = sprintf('Main frequency:   %4.3fHz (%4.2fdB)', ...
     peakfreq, val);
-
-fprintf('Mean frequency:   %4.4fHz (Error: %+4.3f%%)\n\n', ...
+str3 = sprintf('\\bf{Mean frequency:   %4.3fHz} (Error: %+4.2f%%)', ...
     meanfreq, ...
     (meanfreq - Tfreq)/Tfreq * 100);
 
 
-subplot(4,1,[1 2 3]);
-plot(ft,ff);
+disp(str1);
+disp(str2);
+disp(str3);
+fprintf('\n');
+
+
+str = {s1,s2,'',str1,str2,str3};
+plot_fracdiv(ft,ff,str);
 xlim([0 5*Tfreq]);
 ylim([-80 0]);
-xlabel('Frecuencia (Hz)');
-ylabel('Potencia (dB)');
-title('Analisis de frecuencia');
 
-subplot(4,1,4);
-plot(t(1:n/T*3),out(1:n/T*3))
 
